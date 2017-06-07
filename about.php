3a810526e3322ad22206df086cdc520ffe7c1ee4
Arcanae <justin.courdesse@gmail.com>
2017-06-07 16:17:59 +0200

    nav php

---
+++ about.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About</title>
+</head>
+<body>
+    <?php include("nav.php") ?>
+    <main>
+        <header id="aboutheader">
+
+        </header>
+        <section id="aboutcont">
+            <h1 id="welcome">Welcome to Arcanae's page</h1>
+            <p id="indexp">Use the menu at the left to navigate through the site.</p>
+        </section>
+    </main>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,17 @@
+<nav>
+    <div id="logocont">
+        <img id="logo" src="" alt="logo">
+        <p id="title">Arcanae.co</p>
+    </div>
+    <div id="navcont1">
+        <div id="navcont2">
+            <a class="navelement" href="about.php">About</a>
+            <a class="navelement" href="works.php">Works</a>
+            <a class="navelement" href="links.php">Links</a>
+            <a class="navelement" href="contact.php">Contact</a>
+        </div>
+    </div>
+    <footer>
+        <p id="copyright">© Arcanae</p>
+    </footer>
+</nav>
