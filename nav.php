af60252b5155ae80bed40a5bd90654436ea8575a
Arcanae <justin.courdesse@gmail.com>
2017-06-09 09:24:56 +0200

    test .php

--- nav.php
+++ nav.php
@@ -1,11 +1,11 @@
 <nav>
     <div id="logocont">
         <img id="logo" src="" alt="logo">
-        <p id="title">Arcanae.co</p>
+        <p class="title" id="title">Arcanae.co</p>
     </div>
     <div id="navcont1">
         <div id="navcont2">
-            <a class="navelement" href="index.html">Home</a>
+            <a class="navelement" href="index.php">Home</a>
             <a class="navelement" href="about.php">About</a>
             <a class="navelement" href="works.php">Works</a>
             <a class="navelement" href="http://simplonlyon.fr/">Simplon#3</a>
