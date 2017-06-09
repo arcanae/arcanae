e2909c9246b40fc1fb2ac3e2613284a0958ea6dc
Arcanae <justin.courdesse@gmail.com>
2017-06-09 16:09:20 +0200

    nav translation

--- nav.php
+++ nav.php
@@ -5,24 +5,38 @@
     </div>
     <div id="navcont1">
         <div id="navcont2">
-            <a class="navelement" href="index.php">Home</a>
+            <?php 
+                if ($_SESSION['language'] == 'English') {
+                    echo '<a class="navelement" href="index.php">Home</a>';
+                }
+                if ($_SESSION['language'] == 'Français') {
+                    echo '<a class="navelement" href="index.php">Accueil</a>';
+                }
+            ?>    
             <a class="navelement" href="about.php">About</a>
-            <a class="navelement" href="works.php">Works</a>
+            <?php
+                if ($_SESSION['language'] == 'English') {
+                    echo '<a class="navelement" href="works.php">Works</a>';
+                }
+                if ($_SESSION['language'] == 'Français') {
+                    echo '<a class="navelement" href="works.php">Projets</a>';
+                } 
+            ?>   
             <a target="_blank" class="navelement" href="http://simplonlyon.fr/">Simplon#3</a>
         </div>
     </div>
     <footer>
         <?php if ($_SESSION['language'] == 'English') {?>
-        <form action="" method="POST">
-        <input type="hidden" name="language" value="Français">
-        <input class="langbut" id='enbut' type="submit" value="">
-        </form>
+            <form action="" method="POST">
+            <input type="hidden" name="language" value="Français">
+            <input class="langbut" id='enbut' type="submit" value="">
+            </form>
         <?php } ?>
         <?php if ($_SESSION['language'] == 'Français') {?>
-        <form action="" method="POST">
-        <input type="hidden" name="language" value="English">
-        <input class="langbut" id='frbut' type="submit" value="">
-        </form>
+            <form action="" method="POST">
+            <input type="hidden" name="language" value="English">
+            <input class="langbut" id='frbut' type="submit" value="">
+            </form>
         <?php } ?>
         <p id="copyright">© Arcanae</p>
     </footer>
--- works.php
+++ works.php
@@ -1,5 +1,4 @@
 <?php
-    $page = "Works";
     session_start();
     if (!isset($_SESSION['language'])){
         $_SESSION['language'] = 'English';
@@ -7,6 +6,12 @@
     if (isset($_POST['language'])) {
         $language = $_POST['language'];
         $_SESSION['language'] = $language;
+    }
+    if($_SESSION['language'] == 'English') {
+        $page = "Works";
+    }
+    if($_SESSION['language'] == 'Français') {
+        $page = "Projets";
     }
 ?>
 <!DOCTYPE html>
@@ -31,29 +36,29 @@
         foreach($open as $key => $value) {
             if ($key === 0 OR $key === 1) {}
             else {
-            echo '<article class="projectarticle">';
-            echo '<div class="projectcont">';
-            echo '<a href="../works/'.$value.'/" class="projectname">';
-            echo $value;
-            echo '</a>';
-            echo '<a href="../works/'.$value.'/"><img class="projectscreen" src="../works/'.$value.'/screen.png">';
-            echo '</a>';
-            if($_SESSION['language'] == 'English') {
-                $handle = fopen("../works/".$value."/info.txt", "r");
-                $read = fread($handle, filesize("../works/".$value."/info.txt"));
-                $read = htmlspecialchars($read);
-                $read = str_replace("\n", "</br>", $read);
-                echo '<p class="projectp">'.$read.'</p>';
-            }
-            if($_SESSION['language'] == 'Français') {
-                $handle = fopen("../works/".$value."/infofr.txt", "r");
-                $read = fread($handle, filesize("../works/".$value."/infofr.txt"));
-                $read = htmlspecialchars($read);
-                $read = str_replace("\n", "</br>", $read);
-                echo '<p class="projectp">'.$read.'</p>';
-            }
-            echo '</div>';
-            echo '</article>';
+                echo '<article class="projectarticle">';
+                echo '<div class="projectcont">';
+                echo '<a href="../works/'.$value.'/" class="projectname">';
+                echo $value;
+                echo '</a>';
+                echo '<a href="../works/'.$value.'/"><img class="projectscreen" src="../works/'.$value.'/screen.png">';
+                echo '</a>';
+                if($_SESSION['language'] == 'English') {
+                    $handle = fopen("../works/".$value."/info.txt", "r");
+                    $read = fread($handle, filesize("../works/".$value."/info.txt"));
+                    $read = htmlspecialchars($read);
+                    $read = str_replace("\n", "</br>", $read);
+                    echo '<p class="projectp">'.$read.'</p>';
+                }
+                if($_SESSION['language'] == 'Français') {
+                    $handle = fopen("../works/".$value."/infofr.txt", "r");
+                    $read = fread($handle, filesize("../works/".$value."/infofr.txt"));
+                    $read = htmlspecialchars($read);
+                    $read = str_replace("\n", "</br>", $read);
+                    echo '<p class="projectp">'.$read.'</p>';
+                }
+                echo '</div>';
+                echo '</article>';
             }
         }
 
@@ -61,9 +66,5 @@
        ?>
         </section>
     </main>
-    <?php if($_SESSION['language'] == 'Français') {
-    echo "fr";
-    }
-    ?>
 </body>
 </html>
