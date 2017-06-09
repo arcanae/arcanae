a5b398b141e5beb448c38adfc359571853446a9b
Arcanae <justin.courdesse@gmail.com>
2017-06-09 09:27:19 +0200

    titles

--- index.php
+++ index.php
@@ -10,24 +10,7 @@
 </head>
 
 <body>
-    <nav>
-        <div id="logocont">
-            <img id="logo" src="" alt="logo">
-            <p class="title" id="title">Arcanae.co</p>
-        </div>
-        <div id="navcont1">
-            <div id="navcont2">
-                <a class="navelement" href="index.php">Home</a>
-                <a class="navelement" href="about.php">About</a>
-                <a class="navelement" href="works.php">Works</a>
-                <a class="navelement" href="http://simplonlyon.fr/">Simplon#3</a>
-            </div>
-        </div>
-        <footer>
-            <p id="copyright">© Arcanae</p>
-        </footer>
-    </nav>
-
+    <?php include("nav.php") ?>
     <main>
         <header id="indexheader"></header>
         <div id="space"></div>
