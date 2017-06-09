17176e48c572983ed9fdb7f843e9b9f9a454cea1
Arcanae <justin.courdesse@gmail.com>
2017-06-09 09:33:17 +0200

    changes

--- nav.php
+++ nav.php
@@ -8,7 +8,7 @@
             <a class="navelement" href="index.php">Home</a>
             <a class="navelement" href="about.php">About</a>
             <a class="navelement" href="works.php">Works</a>
-            <a class="navelement" href="http://simplonlyon.fr/">Simplon#3</a>
+            <a target="_blank" class="navelement" href="http://simplonlyon.fr/">Simplon#3</a>
         </div>
     </div>
     <footer>
